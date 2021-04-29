986290275caf21b62ddbfdfe4a4a74b885e16109
Manuel-bit <emmanuel_njuguna@outlook.com>
2021-04-29 14:26:55 +0300

    add CV model to Home app

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CariclumVitae
 
 # Register your models here.
+
+class CariclumVitaeAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(CariclumVitae)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class CariclumVitae(models.Model):
+    cv = models.FileField(upload_to="cv")
