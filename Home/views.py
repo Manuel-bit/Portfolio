3f4fd6fc5fedf7fd9618853d1077fdce972f7c75
Manuel-bit <emmanuel_njuguna@outlook.com>
2021-05-06 09:53:55 +0300

    create new Home view and add CV model

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import CariclumVitae
 
 # Create your views here.
 
 def Index(request):
-
+    cariclumvitae = get_object_or_404(CariclumVitae)
+    print(cariclumvitae)
     return render(request,'Home/Index.html')
